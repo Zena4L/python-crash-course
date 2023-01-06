a96b192f04ba5aa02be68e8e04543350f1ef33b8
Clement Bogyah <Zena@Clements-MacBook-Pro.local>
2023-01-06 23:20:51 +0000

    file

---
+++ Basics/list/even.py
@@ -0,0 +1,9 @@
+print("--- even numbers -----")
+even = list(range(2,11,2))
+for num in even:
+    print(num)
+
+print("----odd numbers -----")
+odd = list(range(1,11,3))
+for num in odd:
+    print(num)
