57977c4bbcf4e8767369514772fca0a37c13603f
Clement Bogyah <Zena@Clements-MacBook-Pro.local>
2023-01-06 16:06:07 +0000

    file

---
+++ Basics/list/organize.py
@@ -0,0 +1,15 @@
+companies = ['google','twitter','microsoft','apple','meta']
+
+print("---- original company list -----")
+print(companies)
+
+print("---- sorted order-----")
+companies.sort()
+print(companies)
+
+print("---- reverse order ----")
+companies.reverse()
+print(companies)
+print("---- reverse order ----")
+companies.reverse()
+print(companies)
