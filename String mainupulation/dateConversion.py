a7a8999e5dab6efccdafeb28b92f7383344a9abf
Clement Owireku-Bogyah <zenerbogyah7@gmail.com>
2023-03-14 15:52:43 +0000

    files

---
+++ String mainupulation/dateConversion.py
@@ -0,0 +1,22 @@
+# this output a date format
+
+def dateConv():
+    # input date string in mm/dd/yy format
+    dateStr = input("Enter date in the format (mm/dd/yy) : ")
+    monthStr,dayStr,yearStr = dateStr.split("/")
+
+    # we evaluate a monthly string
+    months = [
+        "Jan","Feb","Mar","Apr","May",
+        "Jun","Jul","Aug","Sept","Oct",
+        "Nov","Dec"
+    ]
+    monthStr = months[int(monthStr) - 1]
+
+    # output the result in the format we want
+    print("The converted date is : ",monthStr,dayStr +",",yearStr)
+
+    # print(monthStr,dayStr,yearStr)
+
+
+dateConv()
---
+++ String mainupulation/monthAbr.py
@@ -0,0 +1,13 @@
+#A program to print the month abbreviation given it number
+
+def monthAbr():
+    # a month lookup tale
+    months = [
+        "Jan","Feb","Mar","Apr","May",
+        "Jun","Jul","Aug","Sept","Oct",
+        "Nov","Dec"
+    ]
+    n = eval(input("Enter a Month number (1 - 12) : "))
+    print("The month abbreviation is : ", months[n-1] + "." )
+
+monthAbr()
